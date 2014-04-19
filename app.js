0a4d817d3a00f53e8c1260cb4bab3bb6106a8763
zcl <289528400@qq.com>
2014-04-19 17:47:15 +0800

    优化 加入负数检查

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ var io = require('socket.io').listen(server);
 app.set('port', process.env.PORT || 8100);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+//app.use(express.json());
+//app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,86 +28,68 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-var qc_ = 0, jc_ = 0, hy_ = 0, bbs_ = 0, zz_ = 0, total_ = 0, tg_ = 0, fc_ = 0;
+
+var NUM = {
+        qc: 0,
+        jc: 0,
+        hy: 0,
+        bbs: 0,
+        zz: 0,
+        fc: 0,
+        tg: 0
+    },
+    TOTALNUM = 0
+    , NumManager = {
+        addNum: function (data) {
+            NUM[data]++;
+            this.totalNum();
+        },
+        subNum: function (data) {
+            NUM[data]--;
+            this.totalNum();
+        },
+        totalNum: function () {
+            for (var i in NUM) {
+                TOTALNUM += NUM[i];
+            }
+        },
+        resetNum: function () {
+            console.log('检查小于0')
+            for (var i in NUM) {
+                if (NUM[i] < 0) {
+                    NUM[i] = 0;
+                    console.log('小于0的有',i)
+                }
+            }
+        }
+    }
 io.enable('browser client minification');
 io.enable('browser client etag');
-io.set('log level', 3);
+io.set('log level', 2);
 io.set('heartbeat interval', '40');
 io.sockets.on('connection', function (socket) {
-
+    NumManager.resetNum();
     socket.on('room', function (room) {
-        console.log('加入：'+room);
+        console.log('加入：' + room);
+        console.log(NUM)
         socket.room = room;
         socket.join(room);
     });
 
     socket.on('channel', function (data) {
         console.log('上线：' + data.msg);
-        switch (data.msg) {
-            case 'qc':
-                qc_++;
-                break;
-            case 'fc':
-                fc_++;
-                break;
-            case 'tg':
-                tg_++;
-                break;
-            case 'jc':
-                jc_++;
-                break;
-            case 'bbs':
-                bbs_++;
-                break;
-            case 'hy':
-                hy_++;
-                break;
-            case 'zz':
-                zz_++;
-                break;
-            default :
-                break;
-        }
-        total_ = total_ + 1;
-        //socket.broadcast.emit("peopleNum", {msg: {qc: qc_, jc: jc_, hy: hy_, bbs: bbs_, zz: zz_, tg: tg_, total: total_, fc: fc_} });
-        io.sockets.in('manager').emit("peopleNum", {msg: {qc: qc_, jc: jc_, hy: hy_, bbs: bbs_, zz: zz_, tg: tg_, total: total_, fc: fc_} });
+        NumManager.addNum(data.msg);
+        io.sockets.in('manager').emit("peopleNum", {msg: {obj: NUM, total: TOTALNUM} });
     });
 
     socket.on('unchannel', function (data) {
         console.log('下线：' + data.msg);
-        switch (data.msg) {
-            case 'qc':
-                qc_--;
-                break;
-            case 'tg':
-                tg_--;
-                break;
-            case 'fc':
-                fc_--;
-                break;
-            case 'jc':
-                jc_--;
-                break;
-            case 'bbs':
-                bbs_--;
-                break;
-            case 'hy':
-                hy_--;
-                break;
-            case 'zz':
-                zz_--;
-                break;
-            default :
-                break;
-        }
-        total_ = total_ - 1;
-        //socket.broadcast.emit("peopleNum", {msg: {qc: qc_, jc: jc_, hy: hy_, bbs: bbs_, zz: zz_, tg: tg_, total: total_, fc: fc_} });
-        io.sockets.in('manager').emit("peopleNum", {msg: {qc: qc_, jc: jc_, hy: hy_, bbs: bbs_, zz: zz_, tg: tg_, total: total_, fc: fc_} });
+        NumManager.subNum(data.msg);
+        io.sockets.in('manager').emit("peopleNum", {msg: {obj: NUM, total: TOTALNUM} });
     });
 
     socket.on('manager_info', function (data) {
         console.log(data);
-        //socket.broadcast.emit("server_info", data);
         if (data.room) {
             io.sockets.in(data.room).emit('server_info', data);
         } else {
@@ -118,7 +100,6 @@ io.sockets.on('connection', function (socket) {
     });
     socket.on('manager_img', function (data) {
         console.log(data);
-        //socket.broadcast.emit("server_img", data);
         if (data.room) {
             io.sockets.in(data.room).emit('server_img', data);
         } else {
@@ -128,6 +109,7 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('disconnect', function () {
         console.log('离开：' + socket.room);
+        console.log(NUM)
         socket.leave(socket.room);
     })
 });
